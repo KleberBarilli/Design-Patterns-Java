1821d018ee86879f4e1efa5522d1de563fcc4c6f
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-26 19:59:04 -0300

    extra discount

--- src/br/com/kb/store/Budget.java
+++ src/br/com/kb/store/Budget.java
@@ -6,6 +6,7 @@ public class Budget {
 
     private BigDecimal amount;
     private int quantityItems;
+    private String situation;
 
     public Budget(BigDecimal amount, int quantityItems) {
         this.amount = amount;
@@ -18,6 +19,20 @@ public class Budget {
 
     public int getQuantityItems() {
         return quantityItems;
+    }
+
+    public void makeExtraDiscount() {
+        BigDecimal extraAmount = BigDecimal.ZERO;
+        if (situation.equals("UNDER_REVIEW")) {
+            extraAmount = new BigDecimal("0.05");
+        } else if (situation.equals("APPROVED")) {
+            extraAmount = new BigDecimal("0.02");
+        }
+        this.amount = this.amount.subtract(extraAmount);
+    }
+
+    public void approve() {
+        this.situation = "APPROVED";
     }
 
 }
