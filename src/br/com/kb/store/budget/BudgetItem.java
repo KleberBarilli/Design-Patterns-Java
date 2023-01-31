893e84316709cdca3b423f9de7e7d17b3754b369
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-30 21:27:21 -0300

    fix typo

--- src/br/com/kb/store/budget/BudgetItem.java
+++ src/br/com/kb/store/budget/BudgetItem.java
@@ -3,13 +3,13 @@ package br.com.kb.store.budget;
 import java.math.BigDecimal;
 
 public class BudgetItem implements Budgetable {
-    private BigDecimal amomut;
+    private BigDecimal amount;
 
-    public BudgetItem(BigDecimal amomut) {
-        this.amomut = amomut;
+    public BudgetItem(BigDecimal amount) {
+        this.amount = amount;
     }
 
     public BigDecimal getAmount() {
-        return amomut;
+        return amount;
     }
 }
